f5217c0f740cdddccdcf413b691ee063fcf21f18
ozcamseyma <65084952+ozcamseyma@users.noreply.github.com>
2022-12-17 00:52:16 +0100

    Add files via upload
    generic

---
+++ repositoryDemo/repositoryDemo/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Validator validator = new Validator();
+        Customer customer = new Customer();
+        validator.validate(customer);
+
+
+
+    }
+}
---
+++ repositoryDemo/repositoryDemo/src/Validator.java
@@ -0,0 +1,9 @@
+public class Validator {
+
+    public <T extends  IEntity> void validate(T entity){
+
+
+    }
+
+
+}
